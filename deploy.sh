da2c4f66e2fe756125e3e57bc48a88cdd73e1872
Kevin Jones <kevinjones@redhat.com>
2019-03-19 14:55:13 -0400

    Updates for full deployment

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,24 @@
 cd ~
 source ~/stackrc
 
-#  -e /home/stack/templates/ceph-environment.yaml \
-#  -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
-#  -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-rgw.yaml \
-#  -e /usr/share/openstack-tripleo-heat-templates/environments/cinder-backup.yaml \
-#  -e /usr/share/openstack-tripleo-heat-templates/environments/services/sahara.yaml \
-
 time openstack overcloud deploy --templates \
   -r /home/stack/templates/roles_data.yaml \
   -n /home/stack/templates/network_data.yaml \
   -e /home/stack/containers-prepare-parameter.yaml \
-  -e /home/stack/templates/node-info.yaml \
   -e /home/stack/templates/timezone.yaml \
   -e /home/stack/templates/cloudname.yaml \
   -e /home/stack/templates/inject-trust-anchor-hiera.yaml \
+  -e /home/stack/templates/node-info.yaml \
   -e /home/stack/templates/network-isolation.yaml \
   -e /home/stack/templates/network-environment.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/services/octavia.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/services/sahara.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/services/barbican.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-rgw.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/cinder-backup.yaml \
+  -e /home/stack/templates/storage-environment.yaml \
+  -e /home/stack/templates/ceph-environment.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/barbican-backend-simple-crypto.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/enable-swap.yaml \
   --timeout 90
